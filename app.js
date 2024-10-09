928a2caad0013f0b016d8e3c3c02ac00fa5f099f
Nobita_Only <nobitanobiofficial98@gmail.com>
2024-10-09 11:10:58 +0545

    some more

--- app.js
+++ app.js
@@ -1,33 +1,82 @@
 import express from "express";
-import dotenv from "dotenv"; 
-import pg from "pg"; 
-import path from "path"; 
-import { fileURLToPath } from "url"; 
-import database from "./config/db.js";
+import dotenv from "dotenv";
+import pg from "pg";
+import path from "path";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import {database} from "./src/config/db.js";
+import { fileURLToPath } from "url";
+import session from "express-session";
 const app = express();
 const port = 3000;
 
+dotenv.config();
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get('/register', (req, res) => {
-    res.render('register.ejs');
-}); 
-
-app.get("/login" , (req, res) => {
-    res.render('login.ejs');
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
 });
 
-app.get("/dashboard" , (req, res) => {
-    res.render('dashboard.ejs');
-})
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/dashboard", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("dashboard.ejs");
+  }
+  res.redirect("/login");
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/dashboard",
+  passport.authenticate("google", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+  })
+);
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/dashboard", 
+    },
+    function verify(accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,16 @@
 import pg from "pg";
 
-export const database = new pg.client({ 
+export const database = new pg.Client({ 
     user: "postgres",
     host: "localhost",
-    database: "taskmanager",
+    database: "Taskmanager",
     password: "123456",
     port: 5432
 
 });
+if (database) { 
+    database.connect();
+    console.log("connected to database");
+}else{ 
+    console.log("failed to connect to database");
+}
