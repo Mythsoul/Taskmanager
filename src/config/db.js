a71d694bf9cd66a31af85daaa0151dc54c99d145
Mythsoul <Mythsoul2023@gmail.com>
2024-10-29 17:55:40 +0545

    nothing

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,8 @@ export const database = new pg.Pool({
     host : process.env.DB_HOST,
     database : process.env.DB_NAME,
     password : process.env.DB_PASSWORD,
-    port : process.env.DB_PORT
+    port : process.env.DB_PORT,
+    
 });
 
 database.connect((err) => {
